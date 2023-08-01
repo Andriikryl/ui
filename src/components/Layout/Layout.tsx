967772e51171dd444b9e30c0484dbbbb44341e47
Andriikryl <andreyk557@gmail.com>
2023-08-01 12:21:10 +0300

    add layout component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+interface ILayoutProps {
+  children?: ReactNode;
+}
+
+export const Layout = ({ children }: ILayoutProps) => {
+  return <div>{children}</div>;
+};
---
+++ src/components/Layout/index.ts
@@ -0,0 +1,1 @@
+export {Layout as deafult} from "./Layout"
